e83841ef9f253a090f3a0243c06eee48ec0be674
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-12-23 17:27:54 +0530

    Module-9

---
+++ Module-9/TCPClient.py
@@ -0,0 +1,28 @@
+import socket
+
+def main():
+	host = '192.168.238.1'
+	port = 5001
+
+	s = socket.socket()
+	s.connect((host, port))
+	print("Connection established..!!!")
+	print(s.recv(1024).decode())
+	message = input("enter the text: ")
+	try:
+		while message != 'q':
+			s.send(message.encode())
+			message = s.recv(1024)
+			print("output : "+message.decode())
+			if message.decode().split(" ")[0] == "Bingo!!":
+				break
+			message = input("Input: ")
+
+		print("Client closed")
+		s.close()
+	except:
+		print("Client closed")
+		s.close()
+
+if __name__ == '__main__':
+	main()
---
+++ Module-9/TCPServer.py
@@ -0,0 +1,47 @@
+import socket
+import random
+import threading
+
+def main():
+	host = '192.168.238.1'
+	port = 5001
+	s= socket.socket()
+	s.bind((host, port))
+	s.listen(1)
+	print("server started..!!")
+	while True:
+		c, addr = s.accept()
+		print("Connection established from: "+ str(addr))
+		c.send("Welcome to the game 'Guess my number!' \n Guess any number of Your choice To start the game! \n Guess between 1 to 50".encode())
+		threading.Thread(target = game, args = (c, addr)).start()
+		# game(c, addr)
+
+def game(c, addr):
+	connect = True
+	num = random.randint(1,51)
+	count = 1
+	while connect:
+		data = c.recv(1024).decode()
+		if data == "Quit":
+			break
+		#Guesser implimentation.
+		data = int(data)
+		print("client "+str(addr)+" guessed: "+str(data))
+		if data == num:
+			String = "Bingo!! You Guessed correct number in: "+str(count)+" attempts"
+			c.send(String.encode())
+			connect = False
+		elif data < num and data >= 0:
+			c.send("Your Guess is  Less than actual number".encode())
+		elif data > num and data <= 100:
+			c.send("Your Guess is Greater than actual number".encode())
+		else:
+			c.send("Enter a valid input: ".encode())
+		count += 1
+	print("server closed from client: "+str(addr))
+	c.close()
+
+	
+
+if __name__ == '__main__':
+	main()
